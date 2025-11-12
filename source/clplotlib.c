00dbd5d72699bd0f9a54fad82f5b699d00a5511e
AlexSKGHA <41026477+AlexSKGHA@users.noreply.github.com>
2025-11-12 11:37:55 +0400

    Update clplotlib.c

--- source/clplotlib.c
+++ source/clplotlib.c
@@ -4,17 +4,13 @@
 
 #include "clplotlib.h"
 
-/*
-	One issue: overflow of resolution in createPlotPlane not handled
-*/
-
 struct PlotPlane createPlotPlane(unsigned int width, unsigned int height) {
-	char *matrix;
-	
-	if (width * height == 0) {
-		fprintf(stderr, "Plane's size is 0\n");
+	if (((width * height) / width) != height) {
+		fprintf(stderr, "Error matrix not jive with dimensions\n");
 		exit(1);
 	}
+
+	char *matrix;
 
 	if ((matrix = malloc(sizeof(char) * height * width)) == NULL) {
 		fprintf(stderr, "Error allocating mem for PlotPlane\n");
@@ -29,16 +25,46 @@ struct PlotPlane createPlotPlane(unsigned int width, unsigned int height) {
 	return pp;
 }
 
+int areCordsWithinRange(struct PlotPlane pp, unsigned int x, unsigned int y) {
+	if ((x < pp.width) && (y < pp.height)) {
+		return 1;
+	} else return 0;
+}
+
+void setPixel(struct PlotPlane pp, unsigned int x, unsigned int y, char texture) {
+	if (areCordsWithinRange(pp, x, y)) {
+		pp.matrix[pp.width * y + x] = texture;
+	} else {
+		fprintf(stderr, "Error setting the pixel out of bounds\n");
+	}
+}
+
 void printPlotPlane(struct PlotPlane pp) {
-	unsigned int widthCounter = 0;
+	unsigned int heightCounter = 0;
 	char *tempStr = calloc(pp.width + 1, sizeof(char));
 	if (tempStr == NULL) {
 		fprintf(stderr, "Error allocating mem to tempStr in printPlotPlane()\n");
 		exit(1);
 	}
-	while (widthCounter < pp.width) {
-		strncpy(tempStr, pp.matrix + pp.width * widthCounter, pp.width);
+	while (heightCounter < pp.height) {
+		strncpy(tempStr, pp.matrix + pp.width * heightCounter, pp.width);
 		printf("%s\n", tempStr);
-		widthCounter++;
+		heightCounter++;
 	}
+}
+
+char *setStringHorizontally(struct PlotPlane pp, unsigned int x, unsigned int y, char *str) {
+	if (!areCordsWithinRange(pp, x, y)) return NULL;
+
+	unsigned int strLength = strlen(str);
+	unsigned int newx = x;
+	for (unsigned int i = 0; i < strLength; i++) {
+		if (newx < pp.width) {
+			setPixel(pp, newx, y, str[i]);
+			newx++;
+		} else {
+			return str + newx;
+		}
+	}
+	return '\0';
 }
